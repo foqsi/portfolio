687b7c4f79a462fa874d4bba3fb0578a5c172e0c
Edward Davis <foqsi03@gmail.com>
2023-09-16 15:16:56 -0500

    added custom submit message and clear form on submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../contact-form.css'
 
-
 const Contact = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://getform.io/f/b62bd12a-3662-484e-860b-01f755153839', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        setIsSubmitted(true);
+        setMessage('// Form submitted successfully.');
+        setFormData({ name: '', email: '', message: '' }); // Clear form
+      } else {
+        setMessage('// Form submission failed.');
+      }
+    } catch (error) {
+      setMessage('// There was an error submitting the form.');
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form method='POST' action="https://getform.io/f/b62bd12a-3662-484e-860b-01f755153839" className='flex flex-col max-w-[600px] w-full'>
+      <form onSubmit={handleSubmit} className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-gray-300'>Contact</p>
           <p className='text-gray-300 py-4'>// Send me a message</p>
         </div>
         <div className='input-group'>
-          <input className='p-2 bg-[#0a192f]' type="text" name='name' required />
+          <input className='p-2 bg-[#0a192f]'
+            type="text"
+            name='name'
+            value={formData.name}
+            onChange={handleChange}
+            required />
           <label for="">Name</label>
         </div>
         <div className='input-group'>
-          <input className='p-2 bg-[#0a192f]' type="email" name='email' required />
+          <input className='p-2 bg-[#0a192f]'
+            type="email"
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+            required />
           <label for="">Email</label>
         </div>
-        <textarea className='mt-4 bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-        <button className='text-white border-2 hover:bg-cyan-400 hover:border-cyan-400 px-4 py-3 my-8 mx-auto flex items-center'>Send Message</button>
+        <textarea className='mt-4 bg-[#ccd6f6] p-2'
+          name="message"
+          rows="10"
+          value={formData.message}
+          onChange={handleChange}
+          placeholder='Message'
+          required />
+
+
+        {!isSubmitted && (
+          <button className='text-white border-2 hover:bg-cyan-400 hover:border-cyan-400 px-4 py-3 my-8 mx-auto flex items-center'>Send Message</button>
+        )}
+        <p className='text-gray-300 px-4 py-3 my-8 mx-auto flex items-center'>{message}</p>
       </form>
     </div>
   )
 }
+
 
 export default Contact
